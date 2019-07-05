382821d742ca987de1797a636133c716ce2ddcee
Jessica Barron <iamjessicabarron@gmail.com>
2019-07-05 12:09:42 +1000

    Added logic for detecting win

--- src/lib/Game.ts
+++ src/lib/Game.ts
@@ -5,21 +5,21 @@ enum Player {
   Computer
 }
 
-export default class Game {
+export class Game {
   static player: Player = Player.User;
-  board: Board = new Board()
+  board: GameBoard = new GameBoard()
 
   constructor() {
     print("Initialising game")
   }
 }
 
-class Board {
+export class GameBoard {
   // board
-  rows = 6
-  columns = 7
+  numberOfRows = 6
+  numberOfColumns = 7
 
-  slots: Slot[] = []
+  private slots: GameSlot[] = []
   
   constructor() {
     this.setup()
@@ -36,9 +36,9 @@ class Board {
 
   // life cycle
   setup() {
-    for (let s = 0; s < this.rows; s++) {
-      for (let c = 0; c < this.columns; c++) {
-        let newSlot = new Slot(s, c)
+    for (let s = 0; s < this.numberOfRows; s++) {
+      for (let c = 0; c < this.numberOfColumns; c++) {
+        let newSlot = new GameSlot(s, c)
         this.slots.push(newSlot)
       }
     }
@@ -48,12 +48,13 @@ class Board {
     this.slots = []
   }
 
-  chooseSlot(rowIndex: number, columnIndex: number): Slot | null {
+  // Function is probably unnecessary
+  private chooseSlot(rowIndex: number, columnIndex: number): GameSlot | null {
     let chosenSlot = this.slots.find(item => item.rowIndex == rowIndex && item.colIndex == columnIndex )
 
     if (chosenSlot) {
       print(`Chose slot at ${chosenSlot.desc}`)
-      chosenSlot.filledBy = Player.Computer
+      chosenSlot.filledBy = Game.player
       this.printBoard()
 
       return chosenSlot
@@ -62,12 +63,43 @@ class Board {
     return null
   }
 
-  checkForConnectFour(arr: Slot[], isRow: Boolean): Boolean {
-    let count = 0
+
+  chooseColumn(selectedSlot: GameSlot) {
+    let col = this.columnFromSlot(selectedSlot)
+    
+    let filledSlot = col
+      .sort((a, b) => a.rowIndex- b.rowIndex) 
+      .filter(slot => slot.filledBy === null)
+      .pop()
+
+    if (filledSlot) {
+      filledSlot.filledBy = Game.player
+      this.checkIfWon(filledSlot)
+    } else {
+      print("Error: Couldn't fill slot")
+    }
+  }
+
+  private checkIfWon(newSlot: GameSlot) {
+    let potentialWins = [
+      this.checkForConnectFour(newSlot, this.rowFromSlot(newSlot), true), // this one relies on row index
+      this.checkForConnectFour(newSlot, this.columnFromSlot(newSlot), false),
+      this.checkForConnectFour(newSlot, this.leftDownDiagonalFromSlot(newSlot), false),
+      this.checkForConnectFour(newSlot, this.rightDownDiagonalFromSlot(newSlot), false)
+    ]
+
+    if (potentialWins.some(win => win)) {
+      print(`WINNER!`)
+    }
+  }
+
+  private checkForConnectFour(newSlot: GameSlot, arr: GameSlot[], isRow: Boolean): Boolean {
+    let count = 1
     let filled = arr
-      .filter(slot => slot.filledBy == Game.player)
+      .filter(slot => slot.filledBy == newSlot.filledBy)
       .map(item => {
-        return new SlotForEvaluation(isRow ? item.rowIndex : item.colIndex)
+        // flip around values when dealing with rows
+        return new SlotInArray(isRow ? item.rowIndex : item.colIndex, isRow ? item.colIndex : item.rowIndex)
       })
       .sort((a, b) => a.index - b.index) // double check it's sorted
     
@@ -76,7 +108,7 @@ class Board {
     }
 
     for (var i = 1; i < filled.length; i++) {
-      if (filled[i].index - filled[i-1].index != 1 ) {
+      if (filled[i].locationIndex - filled[i-1].locationIndex != 1 ) {
         return false
       } 
       count ++
@@ -90,15 +122,15 @@ class Board {
   }
 
   // evaluate relevant slots
-  columnFromSlot(newSlot: Slot): Slot[] {
+  columnFromSlot(newSlot: GameSlot): GameSlot[] {
     return this.slots.filter(slot => slot.colIndex === newSlot.colIndex)
   } 
 
-  rowFromSlot(newSlot: Slot): Slot[] {
+  rowFromSlot(newSlot: GameSlot): GameSlot[] {
     return this.slots.filter(slot => slot.rowIndex === newSlot.rowIndex)
   } 
 
-  leftDownDiagonalFromSlot(newSlot: Slot): Slot[] {
+  leftDownDiagonalFromSlot(newSlot: GameSlot): GameSlot[] {
     return this.slots.filter(slot => {
       let slotSum = slot.rowIndex + slot.colIndex
       let newSlotSum = newSlot.rowIndex + newSlot.colIndex
@@ -106,7 +138,7 @@ class Board {
       return newSlotSum === slotSum
     })
   }
-  rightDownDiagonalFromSlot(newSlot: Slot): Slot[] {
+  rightDownDiagonalFromSlot(newSlot: GameSlot): GameSlot[] {
     return this.slots.filter(slot => {
       let slotSum = slot.rowIndex - slot.colIndex
       let newSlotSum = newSlot.rowIndex - newSlot.colIndex
@@ -117,7 +149,7 @@ class Board {
 
   // printing
   printBoard() {
-    for (let s = 0; s < this.rows; s++) {
+    for (let s = 0; s < this.numberOfRows; s++) {
       let rowString = ""
       let row = this.slots.filter(slot => slot.rowIndex === s)
       row.forEach(item => rowString += item.desc)
@@ -126,7 +158,7 @@ class Board {
     }
   }
 
-  printRowsForEvaluation(chosenSlot: Slot) {
+  printRowsForEvaluation(chosenSlot: GameSlot) {
     // get columns
       print("Row: ")
       print(this.rowFromSlot(chosenSlot).map(item => item.desc).join(""))
@@ -141,14 +173,26 @@ class Board {
       this.leftDownDiagonalFromSlot(chosenSlot).forEach(item => print(item.desc))
   }
 
-}
+  // UI helpers
+  get slotsByRow(): GameSlot[][] {
+    let arr = []
+    for (var i = 0; i < this.numberOfRows; i++) {
+      arr.push(this.slots.filter(item => item.rowIndex === i))
+    }
+    return arr
+  } 
 
+  get numberOfFilledSlots(): number {
+    return this.slots.filter(slot => slot.filledBy !== null).length
+  } 
+
+}
 
 enum SlotState {
   Empty, Filled
 }
 
-class Slot {
+export class GameSlot {
   filledBy: Player | null = null
   rowIndex: number
   colIndex: number
@@ -163,18 +207,20 @@ class Slot {
     this.colLetter = String.fromCharCode(97 + this.colIndex);
   }
 
-    get desc(): string {
-      let empty = `${this.rowIndex}${this.colLetter}`
-      let filled = "  "
-      return `[${this,this.filledBy === null ? empty : filled }]`;
-    } 
+  get desc(): string {
+    let empty = `${this.rowIndex}${this.colLetter}`
+    let filled = "  "
+    return `[${this,this.filledBy === null ? empty : filled }]`;
+  } 
 }
 
 // TODO: Consider if necessary
-class SlotForEvaluation {
+class SlotInArray {
   index: number
+  locationIndex: number
 
-  constructor(index: number) {
-    this.index = index
+  constructor(index: number, locationIndex: number) {
+    this.index = index // row index
+    this.locationIndex = locationIndex
   }
 }
